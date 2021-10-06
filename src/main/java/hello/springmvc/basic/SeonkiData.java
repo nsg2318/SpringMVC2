d2d1a5322277cf95ef3bb11e8512c73896e8db5f
sunba <nsg2318@naver.com>
2021-10-06 23:17:03 +0900

    Http API 복습

---
+++ src/main/java/hello/springmvc/basic/SeonkiData.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic;
+
+
+public class SeonkiData {
+    private String itemName;
+    private String price;
+
+    public SeonkiData(String itemName, String price) {
+        this.itemName = itemName;
+        this.price = price;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -2,13 +2,12 @@ package hello.springmvc.basic.response;
 
 
 import hello.springmvc.basic.HelloData;
+import hello.springmvc.basic.SeonkiData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -52,4 +51,16 @@ public class ResponseBodyController {
         return helloData;
     }
 
+    @GetMapping("/test")
+    public String testData(){
+        return "abcd";
+    }
+
+    //이런 스타일 많이 씀
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @PostMapping("test/post")
+    public SeonkiData HttpApiTest(@RequestBody SeonkiData seonkiData){
+        return seonkiData;
+    }
 }
